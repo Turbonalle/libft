b7db3c3e472a774d8e12c4a212204eb9d5a282f2
jbagger <jbagger@student.hive.fi>
2022-11-01 15:08:14 +0200

    added strjoin and strnstr, changes to strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,25 +3,23 @@
 
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	char *a;
-	char *b;
+    char *a;
+    char *b;
 
-	a = (char *)s1;
-	b = (char *)s2;
+    a = (char *)s1;
+    b = (char *)s2;
 
-	while (n > 0)
-	{
-		if (!*a || !*b)
-		{
-			return (0);
-		}
-		if (*a != *b)
-		{
-			return (*a - *b);
-		}
-		n--;
-	}
-	return (0);
+    while (n > 0 && (*a || *b))
+    {
+        if (*a != *b)
+        {
+            return (*a - *b);
+        }
+        a++;
+        b++;
+        n--;
+    }
+    return (0);
 }
 
 int main()
@@ -29,19 +27,28 @@ int main()
 	char *a = "Hello";
 	char *b = "He";
 
-	int n = strlen(a);
+	//int n = strlen(a);
 	int i = 1;
 	int max = 5;
 
 	while (i <= max)
 	{
-		printf("strncmp(a, b, %d) returns: |%d|\n", i, strncmp(a, b, i));
+		printf("strncmp(%s, %s, %d) returns: |%d|\n", a, b, i, strncmp(a, b, i));
+		i++;
+	}
+
+	printf("\n");
+	
+	i = 1;
+	while (i <= max)
+	{
+		printf("ft_strncmp(%s, %s, %d) returns: |%d|\n", a, b, i, ft_strncmp(a, b, i));
 		i++;
 	}
 
 	printf("\n");
 
-	printf("strcmp(a, b) returns: |%d|\n", strcmp(a, b));
+	printf("strcmp(%s, %s) returns: |%d|\n", a, b, strcmp(a, b));
 
 	return (0);
 }
---
+++ ft_strnstr.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *	ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	char *p1;
+	char *p2;
+	size_t i;
+
+	p1 = (char *)haystack;
+	p2 = (char *)needle;
+	if (!needle)
+	{
+		return(p1);
+	}
+	i = 0;
+	while (p1 && len > 0)
+	{
+		while (p1[i] == *p2)
+		{
+			i++;
+			p2++;
+			if (*p2 == '\0')
+				return (p1);
+		}
+		p1++;
+		len--;
+	}
+	return (0);
+}
+
+int main()
+{
+	char haystack[] = "abcdefg";
+	char needle[] = "def";
+	int len = 10;
+
+	while (len > 0)
+	{
+		printf("strnstr(%s, %s, %d) = '%s'\n", haystack, needle, len, ft_strnstr(haystack, needle, len));
+		len--;
+	}
+	return (0);
+}
