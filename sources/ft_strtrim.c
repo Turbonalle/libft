ed25d39d7319d072351c6ad126d284c41445d414
jbagger <jbagger@student.hive.fi>
2022-11-06 15:47:07 +0200

    Test Passed: strtrim

--- sources/ft_strtrim.c
+++ sources/ft_strtrim.c
@@ -1,83 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int setmatch(char c, char *set)
+size_t ft_strlen(const char *str)
 {
-	while (*set)
+    size_t i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char *ft_strchr(const char *s, int c)
+{
+	char *ptr;
+
+	ptr = (char *)s;
+	while (*ptr)
 	{
-		if (c == *set)
-		{
-			return (1);
-		}
-		set++;
+		if (*ptr == (unsigned char)c)
+			return (ptr);
+		ptr++;
 	}
-	return (0);
+	if (c == 0)
+		return (ptr);
+	return (NULL);
+}
+
+
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t i;
+
+	i = 0;
+	if (dstsize > 0)
+	{
+		while (src[i] && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	while (src[i])
+		i++;
+	return (i);
+}
+
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char *substring;
+	size_t i;
+
+	if (!(substring = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		ft_strlcpy(substring, "", len);
+		return (substring);
+	}
+	i = 0;
+	while (s[start] && len > 0)
+	{
+		substring[i++] = s[start++];
+		len--;
+	}
+	substring[i] = '\0';
+	return (substring);
 }
 
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-	char *ps1;
-	char *pset;
-	char *new;
-	char *stop;
-	
-	ps1 = (char *)s1;
-	pset = (char *)set;
-	while (setmatch(*ps1, pset))
-		ps1++;
-	stop = ps1;
-	while (*stop)
-		stop++;
-	stop--;
-	while (setmatch(*stop, pset))
-		stop--;
-	new = (char *)malloc(sizeof(char) * (stop - ps1 + 1));
-	int i = 0;
-	while (ps1 <= stop)
-	{
-		new[i] = *ps1;
-		i++;
-		ps1++;
-	}
-	new[i] = '\0';
-	return (new);
+	size_t i;
+
+	if (!s1 || !set)
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	i = ft_strlen(s1);
+	if (i == 0)
+		return (ft_substr(s1, 0, i + 1));
+	while (1 && ft_strchr(set, s1[i]))
+		i--;
+	return (ft_substr(s1, 0, i + 1));
 }
 
 
 int main()
 {
-	char *s1 = "aababearabbbb";
-	char *set = "ab";
-	char match = 'a';
-	char nomatch = 'x';
+	printf("----STRTRIM TEST----\n");
+	char *s1 = "  \t \t \n   \n\n\n\t";
+	char *set1 = " \n\t";
+	printf("'%s'\n", ft_strtrim(s1, set1));
 
-	printf("\n----SETMATCH TEST----\n");
+	char *s2 = "\t   \n\n\n  \n\n\t    Hello \t  Please\n Trim me !\t\t\t\n  \t\t\t\t  ";
+	char *set2 = " \n\t";
+	printf("'%s'\n", ft_strtrim(s2, set2));
 
-	printf("%d\n", setmatch(match, s1));
-	printf("%d\n", setmatch(nomatch, s1));
-
-	if (setmatch(match, s1))
-	{
-		printf("The letter matches!\n");
-	}
-	if (!setmatch(match, s1))
-	{
-		printf("There is no such letter\n");
-	}
-
-	if (setmatch(nomatch, s1))
-	{
-		printf("The letter matches!\n");
-	}
-	if (!setmatch(nomatch, s1))
-	{
-		printf("There is no such letter\n");
-	}
-
-	printf("\n\n----STRTRIM TEST----\n");
-	
-	printf("%s\n", ft_strtrim(s1, set));
-	
+	char *s3 = "aeeaHello! Please trim me!eaee";
+	char *set3 = "abcdefghijk";
+	printf("'%s'\n", ft_strtrim(s3, set3));
 	return (0);
 }
